deff3706a4698a3f257444364cab66092a8b2b3e
Diamond Bond <diamond.nigel75@Gmail.com>
2022-04-13 22:58:04 +0530

    update
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- script.py
+++ script.py
@@ -16,17 +16,36 @@ def stripNAN(s):
     i = str(s).replace(", [nan, nan]", "")
     return i
 
+def listToString(s):
+    # initialize an empty string
+    str1 = ""
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+    # return string
+    return str1
+
+
+def stripContainer(s):
+    a = str(s).replace("'", "")
+    b = str(a).replace("[", "")
+    c = str(b).replace("]", "")
+    return c
+
 #filename = sys.argv[1]
 filename = "in.xlsx"
+
 columns = []
-columns.append('S') # Age
-columns.append('T') # Gender
-columns.append('U') # Year
+UserInfo = ['S','T','U']
+ABIS = ['V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH']
+DERS = ['AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ','BA','BR']
+AUDIT = ['BS','BT','BU','BV','BW','BX','BY','BZ','CA','CB']
 
 
 fc = ",".join(columns)
 
-columns_df = pd.read_excel(filename, sheet_name=0, usecols=fc)
+columns_df = pd.read_excel(filename, sheet_name=0, usecols=fc, skiprows=[1])
+#.map({1:5, 2:4, 3:3, 4:2, 5:1})
 list = columns_df.values.tolist()
 
 # Strip [nan] entries
@@ -35,6 +54,9 @@ dictList = eval(cleanedList)
 
 # output columns
 jsonString = json.dumps(dictList)
-jsonFile = open("masterlist.json", "w")
+jsonFile = open("userinfo.json", "w")
 jsonFile.write(jsonString)
-print("\nList formatted and written to masterlist.json")
+#print("\nList formatted and written to userinfo.json")
+
+# output template
+# Participant ID (num) - Gender (text) - Age (num) - Year of study (text) - ABIS (num) - DERS (num) - AUDIT (num)
