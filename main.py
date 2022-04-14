43a4740b8105c2beab1953c831a7c882c52e186a
Diamond Bond <diamond.nigel75@Gmail.com>
2022-04-14 06:03:00 +0530

    updates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 filename = sys.argv[1]
 
 # Columns definition
-columns = ['V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ','BA','BB','BC','BD','BE','BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BW','BX','BY','BZ','CA','CB']
+columns = ['S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ','BA','BB','BC','BD','BE','BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BW','BX','BY','BZ','CA','CB']
 fc = ",".join(columns)
 
 # Setup DataFrame
